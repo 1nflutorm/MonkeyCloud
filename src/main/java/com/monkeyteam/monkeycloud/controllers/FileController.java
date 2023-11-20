53bc8860eacd2443da81942b206d3b91f176cbf8
Egor22083 <egortalanin@gmail.com>
2023-11-20 22:14:42 +0300

    file upload

---
+++ src/main/java/com/monkeyteam/monkeycloud/controllers/FileController.java
@@ -0,0 +1,25 @@
+package com.monkeyteam.monkeycloud.controllers;
+
+import com.monkeyteam.monkeycloud.dtos.FileUploadRequest;
+import com.monkeyteam.monkeycloud.exeptions.AppError;
+import com.monkeyteam.monkeycloud.services.FileService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+@Controller
+@RequiredArgsConstructor
+public class FileController {
+    private final FileService fileService;
+    @PostMapping("/upload")
+    public ResponseEntity<?> uploadFile(@RequestBody FileUploadRequest file) {
+        return fileService.uploadFile(file);
+    }
+}
