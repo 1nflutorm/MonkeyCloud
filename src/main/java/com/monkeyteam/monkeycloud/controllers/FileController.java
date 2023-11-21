0399bf0c3e8a5e5521f30a3f5570e486041c6c83
Egor22083 <egortalanin@gmail.com>
2023-11-21 19:31:09 +0300

    file operations done

--- src/main/java/com/monkeyteam/monkeycloud/controllers/FileController.java
+++ src/main/java/com/monkeyteam/monkeycloud/controllers/FileController.java
@@ -1,26 +1,37 @@
 package com.monkeyteam.monkeycloud.controllers;
 
+import com.monkeyteam.monkeycloud.dtos.FileDownloadRequest;
+import com.monkeyteam.monkeycloud.dtos.FileRenameRequest;
 import com.monkeyteam.monkeycloud.dtos.FileUploadRequest;
-import com.monkeyteam.monkeycloud.exeptions.AppError;
 import com.monkeyteam.monkeycloud.services.FileService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
 public class FileController {
     private final FileService fileService;
+
     @PostMapping("/upload")
     public ResponseEntity<?> uploadFile(@ModelAttribute FileUploadRequest file) {
         return fileService.uploadFile(file);
+    }
+
+    @GetMapping("/download")
+    public ResponseEntity<?> downloadFile(@ModelAttribute FileDownloadRequest file) {
+        return fileService.downloadFile(file);
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<?> deleteFile(@ModelAttribute FileDownloadRequest file) {
+        return fileService.deleteFile(file);
+    }
+
+    @PutMapping("/rename")
+    public ResponseEntity<?> renameFile(@ModelAttribute FileRenameRequest file) {
+        return fileService.renameFile(file);
     }
 }
--- src/main/java/com/monkeyteam/monkeycloud/services/FileService.java
+++ src/main/java/com/monkeyteam/monkeycloud/services/FileService.java
@@ -1,27 +1,31 @@
 package com.monkeyteam.monkeycloud.services;
 
+import com.monkeyteam.monkeycloud.dtos.FileDownloadRequest;
+import com.monkeyteam.monkeycloud.dtos.FileRenameRequest;
 import com.monkeyteam.monkeycloud.dtos.FileUploadRequest;
 import com.monkeyteam.monkeycloud.exeptions.AppError;
-import io.minio.MinioClient;
-import io.minio.PutObjectArgs;
+import io.minio.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.io.IOException;
 import java.io.InputStream;
 
 @Service
 public class FileService {
     private MinioClient minioClient;
+
     @Autowired
-    public void setMinioClient (MinioClient minioClient){
+    public void setMinioClient(MinioClient minioClient) {
         this.minioClient = minioClient;
     }
+
     public ResponseEntity<?> uploadFile(FileUploadRequest fileUploadRequest) {
         InputStream inputStream = null;
-        try{
+        try {
             inputStream = fileUploadRequest.getMultipartFile().getInputStream();
             minioClient.putObject(PutObjectArgs.builder()
                     .bucket(fileUploadRequest.getUsername())
@@ -37,5 +41,56 @@ public class FileService {
             ioException.printStackTrace();
         }
         return ResponseEntity.ok("Файл загружен корректно");
+    }
+
+    public ResponseEntity<?> downloadFile(FileDownloadRequest fileDownloadRequest) {
+        InputStream inputStream = null;
+        try {
+            inputStream = minioClient.getObject(GetObjectArgs
+                    .builder()
+                    .bucket(fileDownloadRequest.getUsername())
+                    .object(fileDownloadRequest.getFullPath())
+                    .build());
+        } catch (Exception e) {
+            return new ResponseEntity<>(new AppError(HttpStatus.BAD_REQUEST.value(), "Ошибка при скачивании файла"), HttpStatus.BAD_REQUEST);
+        }
+        try {
+            inputStream.close();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+        return ResponseEntity.ok("Файл скачен успешно");
+    }
+
+    public ResponseEntity<?> deleteFile(FileDownloadRequest fileDeleteRequest) {
+        try {
+            minioClient.removeObject(RemoveObjectArgs
+                    .builder()
+                    .bucket(fileDeleteRequest.getUsername())
+                    .object(fileDeleteRequest.getFullPath())
+                    .build());
+        } catch (Exception e) {
+            return new ResponseEntity<>(new AppError(HttpStatus.BAD_REQUEST.value(), "Ошибка при удалении файла"), HttpStatus.BAD_REQUEST);
+        }
+        return ResponseEntity.ok("Файл удалён успешно");
+    }
+    @Transactional
+    public ResponseEntity<?> renameFile(FileRenameRequest fileRenameRequest) {
+        try {
+            minioClient.copyObject(CopyObjectArgs
+                    .builder()
+                    .bucket(fileRenameRequest.getUsername())
+                    .object(fileRenameRequest.getFullPath() + fileRenameRequest.getNewName())
+                    .source(CopySource
+                            .builder()
+                            .bucket(fileRenameRequest.getUsername())
+                            .object(fileRenameRequest.getFullPath() + fileRenameRequest.getOldName())
+                            .build())
+                    .build());
+            deleteFile(new FileDownloadRequest(fileRenameRequest.getUsername(), fileRenameRequest.getFullPath() + "/" + fileRenameRequest.getOldName()));
+        } catch (Exception e) {
+            return new ResponseEntity<>(new AppError(HttpStatus.BAD_REQUEST.value(), "Ошибка при переименовании файла"), HttpStatus.BAD_REQUEST);
+        }
+        return ResponseEntity.ok("Файл переименован успешно");
     }
 }
